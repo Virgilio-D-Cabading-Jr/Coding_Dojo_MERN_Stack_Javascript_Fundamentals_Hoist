cf638d6263aa48ca5750b2b888e7d2dd659300bb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-06 06:20:29 -0800

    add ES6 code to number 1

--- main.js
+++ main.js
@@ -4,3 +4,20 @@
 
 console.log("****************************************");
 console.log("Number 1\n");
+
+// Original Code:
+// console.log(hello);
+// var hello = 'world';
+// Ouput:   undefined
+
+Hoisted Code:
+var hello;
+console.log(hello);
+hello = 'world';
+// Output:  undefined
+
+// ES6 Code:
+// console.log(hello);
+// let hello = 'worrld';
+// Output:  Syntax error: cannot access hello before initialization
+
