37471184ed0ff0fd9c9ac418952b5b4bf955b781
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-07 10:21:43 -0800

    add original code/prediction to number 4

--- main.js
+++ main.js
@@ -88,3 +88,22 @@ console.log(brendan);
 
 console.log("\n****************************************");
 console.log("Number 4\n");
+
+// Original Code
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+// Output:  chicken
+//          half-chicken
+
+
+
+// Hoisted Code
+
+
+// ES6 Code
